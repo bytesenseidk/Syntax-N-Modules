b16fc0db784db0e4faf4a90ccb042c91824905d7
rosenkilde <50791042+rosenkilde95@users.noreply.github.com>
2020-07-30 16:39:16 +0200

    Add files via upload

---
+++ secure_pdf.py
@@ -0,0 +1,21 @@
+from PyPDF2 import PdfFileWriter, PdfFileReader
+
+def secure_pdf(file, password):
+    parser = PdfFileWriter()
+    pdf = PdfFileReader(file)
+
+    for page in range(pdf.numPages):
+        parser.addPage(pdf.getPage(page))
+    parser.encrypt(password)
+
+    with open(f"encrypted_{file}", "wb") as f:
+        parser.write(f)
+        f.close()
+    print(f"encrypted_{file} Created...")
+
+if __name__ == "__main__":
+    file = "kivy.pdf"
+    password = "python_genius"
+    secure_pdf(file, password)
+
+    
