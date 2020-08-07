7b215fd67c194ee44c26aa2d867afa58bbe63320
rosenkilde <50791042+rosenkilde95@users.noreply.github.com>
2020-08-07 19:28:11 +0200

    Create zipper.py

---
+++ zipper.py
@@ -0,0 +1,29 @@
+import os
+from zipfile import ZipFile
+
+def zip_me(path):
+    """ Target folder path & name """
+    folder_name = path.split("\\")[-1]
+    rel_path = path.strip(folder_name)
+
+    """ Save zipfile to the target folder path """
+    os.chdir(rel_path)
+
+    parse = ZipFile(f"{folder_name}.zip", "w")
+
+    for root, dirs, files in os.walk(folder_name, topdown=False):
+        """ Files in sub-folders """
+        for name in files:
+            file = os.path.join(root, name)
+            parse.write(file)
+
+        """ Sub-folders in target folder """
+        for name in dirs:
+            folders = os.path.join(root, name)
+            parse.write(folders)
+    parse.close()
+        
+if __name__ == "__main__":
+    path = input(r"Path to target folder: ")
+    zip_me(path)
+    
