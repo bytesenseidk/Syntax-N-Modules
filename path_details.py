28fefb57b048fae9ba2926539349cc462381cafe
rosenkilde <50791042+rosenkilde95@users.noreply.github.com>
2020-10-10 10:31:20 +0200

    Create path_details.py

---
+++ path_details.py
@@ -0,0 +1,23 @@
+
+
+class Path_Worker(object):
+    def __init__(self, file_path):
+        self.file = file_path
+        self.file_path = file_path.split("\\")
+        self.file_name = self.file_path[-1]
+        self.dir_letter = self.file_path[0] + "\\"
+        self.file_ext = self.file_name.split(".")[-1]
+        
+    def __repr__(self):
+        return str("\n"
+            f"Drive Letter:   {self.dir_letter}\n"
+            f"Origional Path: {self.file}\n"
+            f"File Name:      {self.file_name}\n"
+            f"File Extension: {self.file_ext}")
+
+if __name__ == "__main__":
+    print("Drop a file here: ")
+    file = input(r"  >> ").strip('"')
+    print(Path_Worker(file))
+    
+    
