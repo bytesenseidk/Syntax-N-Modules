b98b5189054e56625b19e06a72b25812b9d16236
rosenkilde <50791042+rosenkilde95@users.noreply.github.com>
2020-10-30 19:20:50 +0100

    Add files via upload

---
+++ instagram_followers.py
@@ -0,0 +1,17 @@
+
+import requests
+
+def followers(username):
+    url = 'https://www.instagram.com/' + username
+    r = requests.get(url).text
+    start = '"edge_followed_by":{"count":'
+    end = '},"followed_by_viewer"'
+    followers = str(r[r.find(start)+len(start):r.rfind(end)])
+    result = f"{username} currently have {int(followers):,} followers."
+    return result
+
+if __name__ == "__main__":
+    print("Enter Instagram Username")
+    username = input("  >> ")
+    print(followers(username))
+
