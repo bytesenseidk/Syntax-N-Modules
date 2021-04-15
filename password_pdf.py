5ef86cac92004410c7dbbf85a916097df4962b26
LarsRosenkilde <50791042+LarsRosenkilde@users.noreply.github.com>
2021-04-15 17:11:44 +0200

    Update

--- password_pdf.py
+++ password_pdf.py
@@ -1,24 +1,27 @@
-from fpdf import FPDF
 from PyPDF2 import PdfFileWriter, PdfFileReader
 
 
-def secure_pdf(file, password):
-    parser = PdfFileWriter()
-    pdf = PdfFileReader(file)
-    # Read in the pdf file
-    for page in range(pdf.numPages):
-        # For every page in the pdf file, add that to the new file
-        parser.addPage(pdf.getPage(page))
-    # Encrypts the pdf, which enable the password Protect feature  
-    parser.encrypt(password)
-    # Opens the encrypted file, 
-    with open(f"encrypted_{file}", "wb") as f:
-        parser.write(f)
-        f.close()
-    print(f"encrypted_{file} Created...")
+class PasswordProtect_PDF(object):
+    def __init__(self, file_path, password="toor"):
+        self.file_path = file_path
+        self.password = password
+        self.writer = PdfFileWriter()
+        self.pdf = PdfFileReader(file_path)
+    
+    
+    def apply(self):
+        for page in range(self.pdf.numPages):
+            self.writer.addPage(self.pdf.getPage(page))
+        self.writer.encrypt(self.password)
+
+        with open(f"Secure_{self.file_path}", "wb") as file:
+            self.writer.write(file)
+            file.close()
+
+        print(f"Secure_{self.file_path} Created...")
+
 
 if __name__ == "__main__":
-    file = "kivy.pdf"
-    password = "python_genius"
-    secure_pdf(file, password)
+    protect = PasswordProtect_PDF("pdf_file.pdf", "python_genius")
+    protect.apply()
 
--- secure_pdf.py
+++ secure_pdf.py
@@ -19,3 +19,4 @@ if __name__ == "__main__":
     secure_pdf(file, password)
 
     
+    
