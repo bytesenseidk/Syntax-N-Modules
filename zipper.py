013c76e51a76f29f51df6ea72bf7189331f9018a
LarsRosenkilde <50791042+LarsRosenkilde@users.noreply.github.com>
2021-06-01 14:50:19 +0200

    Update zipper.py

--- zipper.py
+++ zipper.py
@@ -27,19 +27,4 @@ def zip_me(path):
 if __name__ == "__main__":
     path = input(r"Path to target folder: ")
     zip_me(path)
-    
 
-# import os
-# import zipfile
-    
-# def zipdir(path, ziph):
-#     # ziph is zipfile handle
-#     for root, dirs, files in os.walk(path):
-#         for file in files:
-#             ziph.write(os.path.join(root, file), 
-#                        os.path.relpath(os.path.join(root, file), 
-#                                        os.path.join(path, '..')))
-      
-# zipf = zipfile.ZipFile('Python.zip', 'w', zipfile.ZIP_DEFLATED)
-# zipdir('tmp/', zipf)
-# zipf.close()
