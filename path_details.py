cfa88a64027cd8cd45df09d5aa32e043ab438489
LarsRosenkilde <50791042+LarsRosenkilde@users.noreply.github.com>
2021-06-07 06:03:03 +0200

    Update path_details.py

--- path_details.py
+++ path_details.py
@@ -14,8 +14,9 @@ class Path_Worker(object):
             f"File Extension: {self.file_ext}")
 
 if __name__ == "__main__":
-    print("Drop a file here: ")
-    file = input(r"  >> ").strip('"')
-    print(Path_Worker(file))
+    # print("Drop a file here: ")
+    # file = input(r"  >> ").strip('"')
+    file = r"C:\Users\Anonymous\Documents\GitHub\Instagram-Posted-Sources\path_details.py"
+    print(Path_Worker(file) + "\n\n")
     
     
