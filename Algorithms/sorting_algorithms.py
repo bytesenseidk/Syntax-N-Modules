6d127eb9343b82bf5fe26fc64c45b1d7dd6af991
LarsRosenkilde <50791042+LarsRosenkilde@users.noreply.github.com>
2022-08-12 11:09:17 +0200

    Syntax

--- Algorithms/sorting_algorithms.py
+++ Algorithms/sorting_algorithms.py
@@ -2,6 +2,7 @@ class Sort_Algorithms(object):
     def __init__(self):
         self.sort_me = [9, 2, 1, 4, 7, 6, 5, 3, 8]
 
+
     def __str__(self):
         return str(f"\t[ Sorting Algorithm Demo ]\n" 
             f"Unsorted List:  {self.sort_me}\n"
@@ -11,6 +12,7 @@ class Sort_Algorithms(object):
             f"Heap Sort:      {self.heap_sort(self.sort_me)}\tBig-O: O(nlog(n))\n"
             f"Merge Sort:     {self.merge_sort(self.sort_me)}\tBig-O: O(nlog(n))\n"
             f"Quick Sort:     {self.quick_sort(self.sort_me)}\tBig-O: O(n)")
+
 
     def bubble_sort(self, nums):
         swapped = True
@@ -22,6 +24,7 @@ class Sort_Algorithms(object):
                     swapped = True
         return nums
 
+
     def selection_sort(self, nums):
         for i in range(len(nums)):
             lowest_value_index = i
@@ -30,6 +33,7 @@ class Sort_Algorithms(object):
                     lowest_value_index = j
             nums[i], nums[lowest_value_index] = nums[lowest_value_index], nums[i]
         return nums
+
 
     def insertion_sort(self, nums):
         for i in range(1, len(nums)):
@@ -41,40 +45,36 @@ class Sort_Algorithms(object):
             nums[j + 1] = item_to_insert
         return nums
     
+    
     def heap_sort(self, nums):
         def heapify(nums, heap_size, root_index):
             largest = root_index
             left_child = (2 * root_index) + 1
             right_child = (2 * root_index) + 2
-
             if left_child < heap_size and nums[left_child] > nums[largest]:
                 largest = left_child
-
             if right_child < heap_size and nums[right_child] > nums[largest]:
                 largest = right_child
-
             if largest != root_index:
                 nums[root_index], nums[largest] = nums[largest], nums[root_index]
                 heapify(nums, heap_size, largest)
 
         def heap_sorter(nums):
             n = len(nums)
-
             for i in range(n, -1, -1):
                 heapify(nums, n, i)
-
             for i in range(n - 1, 0, -1):
                 nums[i], nums[0] = nums[0], nums[i]
                 heapify(nums, i, 0)
         heap_sorter(nums)
         return nums
 
+
     def merge_sort(self, nums):
         def merge(left_list, right_list):
             sorted_list = []
             left_list_index = right_list_index = 0
             left_list_length, right_list_length = len(left_list), len(right_list)
-
             for _ in range(left_list_length + right_list_length):
                 if left_list_index < left_list_length and right_list_index < right_list_length:
                     if left_list[left_list_index] <= right_list[right_list_index]:
@@ -91,6 +91,7 @@ class Sort_Algorithms(object):
                     left_list_index += 1
             return sorted_list
 
+
         def merge_sorter(nums):
             if len(nums) <= 1:
                 return nums
@@ -100,6 +101,7 @@ class Sort_Algorithms(object):
             return merge(left_list, right_list)
         merge_sorter(nums)
         return nums
+    
     
     def quick_sort(self, nums):
         def partition(nums, low, high):
@@ -117,16 +119,17 @@ class Sort_Algorithms(object):
                     return j
                 nums[i], nums[j] = nums[j], nums[i]
 
+
         def quick_sorter(nums):
             def _quick_sorter(items, low, high):
                 if low < high:
                     split_index = partition(items, low, high)
                     _quick_sorter(items, low, split_index)
                     _quick_sorter(items, split_index + 1, high)
-
             _quick_sorter(nums, 0, len(nums) - 1)
         quick_sorter(nums)
         return nums
+
 
 if __name__ == "__main__":
     print(Sort_Algorithms())
