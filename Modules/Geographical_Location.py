e646856fc27a5cf7bdacad24e31b8c47744b3c9e
LarsRosenkilde <50791042+LarsRosenkilde@users.noreply.github.com>
2022-08-13 11:37:46 +0200

    Syntax update

--- Modules/Geographical_Location.py
+++ Modules/Geographical_Location.py
@@ -1,13 +1,23 @@
 import geocoder
 import reverse_geocoder as rg
 
-
-def location():
-    latitude, longitude = [*geocoder.ip("me").latlng]
-    city = city = rg.search((latitude, longitude), verbose=False)
-    return city[0]["name"]
-
+class IpLocator(object):
+    def __init__(self, target=geocoder.ip("me")):
+        self.data = self.locate(target)
+    
+    def __str__(self):
+        return str(f"\t[ IP Locator ]\n"
+            f"IP:       {self.data[0]}\n"
+            f"City:     {self.data[1]}\n"
+            f"Country:  {self.data[2]}\n"
+            f"Location: {self.data[3]}")
+        
+    def locate(self, target):
+        latitude, longitude = [*target.latlng]
+        city = rg.search((latitude, longitude), verbose=False)
+        return [target.ip, city[0]["name"], target.country, [latitude, longitude]]
 
 if __name__ == "__main__":
-    print("Your current location: " + location())
-
+    custom_ip = geocoder.ip("8.8.8.8")
+    locate = IpLocator(custom_ip)
+    print(locate)
