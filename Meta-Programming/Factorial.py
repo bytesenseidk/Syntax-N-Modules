7ecac677689c1f3365c8d20f818eaffed1adb47b
Byte Sensei <50791042+bytesenseidk@users.noreply.github.com>
2023-12-03 21:53:16 +0100

    Factorial meta method

---
+++ Meta-Programming/Factorial.py
@@ -0,0 +1,22 @@
+class Factorial(object):
+    def __init__(self, num):
+        self.value = 1
+        # Multiplies each number in num range with __mul__ method
+        for i in range(1, num + 1):
+            self *= i
+        
+    def __mul__(self, other):
+        # Returns a new instance of the Factorial class with updated values
+        result = Factorial(0)
+        result.value = self.value * other.value
+        return result
+    
+    def __repr__(self):
+        # Provices a string representation of the object
+        return str(self.value)
+    
+if __name__ == "__main__":
+    factorial = Factorial(5)
+    print(f"Factorial of 5: {factorial}")
+
+    
