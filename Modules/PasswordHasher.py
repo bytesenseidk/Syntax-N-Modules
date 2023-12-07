e1dd8969cfdf8a313037129ac97060c44d7b6102
Byte Sensei <50791042+bytesenseidk@users.noreply.github.com>
2023-12-07 11:08:37 +0100

    Update

--- Modules/PasswordHasher.py
+++ Modules/PasswordHasher.py
@@ -21,12 +21,14 @@ class HashingDemo(object):
         return str(self.verify_password(self.hashed_password, self.comparer))
 
 if __name__ == "__main__":
-    pass_0 = "Password"
+    pass_0 = "P4ssw0rd"
     pass_1 = "12345678"
-    true_ins = HashingDemo(pass_0, "Password")
-    false_ins = HashingDemo(pass_1, "Password")
+    check_against = "P4ssw0rd"
+    true_ins = HashingDemo(pass_0, check_against)
+    false_ins = HashingDemo(pass_1, check_against)
     true_hash = true_ins.hashed_password.split(',p=')[-1]
     false_hash = false_ins.hashed_password.split(',p=')[-1]
-    print(f"Instance_0: {pass_0} = {true_hash} = {true_ins}\nInstance_1: {pass_1} = {false_hash} = {false_ins}")
+    print(f"Instance_0: Password = {pass_0} | Hash = {true_hash} | {pass_0} = {check_against} == {true_ins}")
+    print(f"Instance_1: Password = {pass_1} | Hash = {false_hash} | {pass_1} = {check_against} == {false_ins}")
     
 
