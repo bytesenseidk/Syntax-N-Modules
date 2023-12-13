cc7c7d672f58999618b7cd27d98eac976c9b466e
Byte Sensei <50791042+bytesenseidk@users.noreply.github.com>
2023-12-13 17:48:22 +0100

    Small update

--- Modules/Screenshot.py
+++ Modules/Screenshot.py
@@ -1,14 +1,16 @@
 import os
 import pyautogui
 
+
 def screenshot():
-    # Save screenshot to picture folder
+    # Save screenshot to picture folder inside your home folder
     save_path = os.path.join(os.path.expanduser("~"), "Pictures")
     shot = pyautogui.screenshot()
+    # Save screenshot in set save_path
     shot.save(f"{save_path}\\python_screenshot.png")
     return print(f"\nScreenshot taken, and saved to {save_path}")
 
+
 if __name__ == "__main__":
     screenshot()
-
 
