3d0db96896ac1c9f9fc0a84f849fc0b69a003d56
Byte Sensei <50791042+bytesenseidk@users.noreply.github.com>
2023-12-31 15:54:10 +0100

    Indent fix

--- Modules/TurtleBasics.py
+++ Modules/TurtleBasics.py
@@ -9,8 +9,9 @@ color_change = 256
 pos = 0
 
 for i in range(512):
-    color = colorsys.hsv_to_rgb(pos, 1, 1)
-	   pos = pos + 1 / color_change
-	   draw.color(color)
-	   draw.forward(i ** 1.5)
-	   draw.left(145)
+	color = colorsys.hsv_to_rgb(pos, 1, 1)
+	pos = pos + 1 / color_change
+	draw.color(color)
+	draw.forward(i ** 1.5)
+	draw.left(145)
+
